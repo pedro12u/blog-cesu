c7d7d451ad9394b3ab0e932b7bee0f138c269487
Pedro Toscano <toscanopedroall@gmail.com>
2024-09-25 14:42:33 -0300

    Root server tests, database, and using prettier in .ejs to tests

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 import mysql from "mysql2/promise";
 
 const app = express();
 
 // Configura EJS como a engine de renderização de templates
-app.set('view engine', 'ejs');
-app.set('views', `${__dirname}/views`);
+app.set("view engine", "ejs");
+app.set("views", `${__dirname}/views`);
 
 const connection = mysql.createPool({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "mudar123",
-    database: "unicesumar"
+  host: "localhost",
+  port: 3000,
+  user: "root",
+  password: "pedro123",
+  database: "unknow_peter_sql",
 });
 
 // Middleware para permitir dados no formato JSON
@@ -20,31 +20,34 @@ app.use(express.json());
 // Middleware para permitir dados no formato URLENCODED
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/categories', async function (req: Request, res: Response) {
-    const [rows] = await connection.query("SELECT * FROM categories");
-    return res.render('categories/index', {
-        categories: rows
-    });
+app.get("/categories", async function (req: Request, res: Response) {
+  const [rows] = await connection.query("SELECT * FROM categories");
+  return res.render("categories/index", {
+    categories: rows,
+  });
 });
 
 app.get("/categories/form", async function (req: Request, res: Response) {
-    return res.render("categories/form");
+  return res.render("categories/form");
 });
 
-app.post("/categories/save", async function(req: Request, res: Response) {
-    const body = req.body;
-    const insertQuery = "INSERT INTO categories (name) VALUES (?)";
-    await connection.query(insertQuery, [body.name]);
+app.post("/categories/save", async function (req: Request, res: Response) {
+  const body = req.body;
+  const insertQuery = "INSERT INTO categories (name) VALUES (?)";
+  await connection.query(insertQuery, [body.name]);
 
-    res.redirect("/categories");
+  res.redirect("/categories");
 });
 
-app.post("/categories/delete/:id", async function (req: Request, res: Response) {
+app.post(
+  "/categories/delete/:id",
+  async function (req: Request, res: Response) {
     const id = req.params.id;
     const sqlDelete = "DELETE FROM categories WHERE id = ?";
     await connection.query(sqlDelete, [id]);
 
     res.redirect("/categories");
-});
+  }
+);
 
-app.listen('3000', () => console.log("Server is listening on port 3000"));
+app.listen("3000", () => console.log("Server is listening on port 3000"));
